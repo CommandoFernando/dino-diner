1fdbc4467a241e801cc64e5e42f8a777f5a6f84e
Fernando Velarde <f.velarde@hotmail.com>
2019-11-17 15:36:08 -0600

    Website Milestone 1 done except for combo in the last step (list the items)

--- Website/Pages/About.cshtml.cs
+++ Website/Pages/About.cshtml.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 namespace Website.Pages
 {
-    public class IndexModel : PageModel
+    public class AboutModel : PageModel
     {
         public void OnGet()
         {
---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
+using DinoDiner;
+
+namespace Website.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public Menu menu = new Menu();
+        /// <summary>
+        /// Getter for menu
+        /// </summary>
+        public Menu Menu
+        {
+            get
+            {
+                return new Menu();
+            }
+        }
+        public List<Drink> ListDrinks()
+        {
+            return Menu.AvailableDrinks;
+        }
+    }
+}
